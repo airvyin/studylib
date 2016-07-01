c3ce121bfd31abe3f75fa7b6498055c6332117b2
airvyin <airv.yin@qq.com>
2016-07-01 13:09:21 +0800

    加入文件

---
+++ src/com/download/util/Contents.java
@@ -0,0 +1,11 @@
+package com.download.util;
+
+import java.io.File;
+
+public interface Contents {
+
+    public final String TASK_FILE_PATH = "C:" + File.separator + "Users" + File.separator + "yintw" + File.separator
+            + "Pictures" + File.separator + "comics" + File.separator + "20160621.txt";
+
+    public final String TASK_FILE_NAME = "taskFileName";
+}
